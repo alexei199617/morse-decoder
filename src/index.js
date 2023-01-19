0d98d8341432f1ae8de10557337d9981d434db8b
alexei199617 <44381914+alexei199617@users.noreply.github.com>
2023-01-19 12:18:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let someText = '';
+    let fullText = '';
+    let count = 0;
+    for (let i = 0; expr.length>i ; i+=2) {
+        if (count == 10) {
+            console.log(someText)
+            if (someText == '*****') {
+                fullText += ' ';
+            } else {
+                fullText += MORSE_TABLE[someText];
+            }
+            console.log(fullText)
+            count = 0;
+            someText = '';
+        }
+        let a = expr.slice(i,(i+2));
+        if (a==10) {
+            someText += '.';
+            count +=2;
+        } else if (a==11) {
+            someText += '-';
+            count +=2;
+        } else if (a == 00) {
+            count +=2;
+        } else if (a == '**') {
+            someText += '*';
+            count +=2;
+        }
+    }
+    return fullText
 }
 
 module.exports = {
