e255ed33fceb76af895926ac143d7e5e12693254
alexei199617 <44381914+alexei199617@users.noreply.github.com>
2023-01-19 12:36:28 +0300

    fix: remove some lines

--- src/index.js
+++ src/index.js
@@ -45,16 +45,12 @@ function decode(expr) {
         let a = expr.slice(i,(i+2));
         if (a==10) {
             someText += '.';
-            count +=2;
         } else if (a==11) {
             someText += '-';
-            count +=2;
-        } else if (a == 00) {
-            count +=2;
         } else if (a == '**') {
             someText += '*';
-            count +=2;
         }
+        count +=2;
         if (count == 10) {
             if (someText == '*****') {
                 fullText += ' ';
